4835e7eb26bcf01fec6083ed969731147b1dfa32
Anastasia Baeramshina <khlebalova1996@mail.com>
2025-08-31 22:34:11 +0500

    ADD Symbols

--- src/main/java/com/javarush/baeramshina/ConsoleRunner.java
+++ src/main/java/com/javarush/baeramshina/ConsoleRunner.java
@@ -1,7 +1,42 @@
 package com.javarush.baeramshina;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class ConsoleRunner {
     public static void main(String[] args) {
 
+        Symbols.printAlphabet();
+
+    }
+
+
+    public class Symbols {
+        private static final char[] ALPHABET = new char[]{'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з',
+                'и', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ',
+                'ъ', 'ы', 'ь', 'э', 'я', '.', ',', '«', '»', '"', '\'', ':', '!', '?', ' '};
+
+        public static void printAlphabet() {
+            for (char c : ALPHABET) {
+                System.out.print(c + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+    private static void readFile() {
+        String input = "C:\\Users\\Microsoft\\IdeaProjects\\CryptoAnalyzerPantera\\text\\dict.txt";
+        try (BufferedReader reader = new BufferedReader(new FileReader(input))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка при чтении файла!!!");
+            e.getMessage();
+        }
     }
 }
