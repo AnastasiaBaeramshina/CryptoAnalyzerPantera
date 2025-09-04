eccc76138724d0b77d1542abac2639d442955096
Anastasia Baeramshina <khlebalova1996@mail.com>
2025-09-04 23:11:24 +0500

    ADD logical changes

--- src/main/java/com/javarush/baeramshina/Encrypt.java
+++ src/main/java/com/javarush/baeramshina/Encrypt.java
@@ -6,11 +6,25 @@ public class Encrypt {
     public static String encryptText(String text, Map<Character, Character> cipherMap) {
         StringBuilder result = new StringBuilder();
         for (char c : text.toCharArray()) {
-            char lowerChar = Character.toLowerCase(c);
-            char encryptedChar = cipherMap.getOrDefault(lowerChar, c);
-            result.append(encryptedChar);
+            if (cipherMap.containsKey(c)) {
+                result.append(cipherMap.get(c));
+            } else {
+                result.append(c);
+            }
         }
         return result.toString();
-
     }
 }
+
+
+//        char lowerChar = Character.toLowerCase(c);
+//        char encryptedChar = cipherMap.getOrDefault(lowerChar, c);
+//        if (Character.isUpperCase(c)) {
+//            encryptedChar = Character.toUpperCase(encryptedChar);
+//        }
+//        result.append(encryptedChar);
+//    }
+//        return result.toString();
+
+//}
+//        }
