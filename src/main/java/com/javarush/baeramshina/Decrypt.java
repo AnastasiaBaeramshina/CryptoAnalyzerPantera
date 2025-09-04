dc8eebc5e35c990b867729065c052e868277d376
Anastasia Baeramshina <khlebalova1996@mail.com>
2025-09-04 23:18:53 +0500

    DEL Decr one of the options

--- src/main/java/com/javarush/baeramshina/Decrypt.java
+++ src/main/java/com/javarush/baeramshina/Decrypt.java
@@ -3,7 +3,7 @@ package com.javarush.baeramshina;
 import java.util.Map;
 
 public class Decrypt {
-    public static String decryptText(String text,Map<Character, Character> cipherMap) {
+    public static String decryptText(String text, Map<Character, Character> cipherMap) {
         StringBuilder result = new StringBuilder();
         for (char c : text.toCharArray()) {
             if (cipherMap.containsKey(c)) {
@@ -21,18 +21,4 @@ public class Decrypt {
 
 
 
-//        StringBuilder result = new StringBuilder();
-//        for (char c : encryptText.toCharArray()) {
-//            char lowerChar = Character.toLowerCase(c);
-//
-//            char decryptedChar = otherCipherMap.getOrDefault(lowerChar, c);
-//            if (Character.isUpperCase(c)) {
-//                decryptedChar = Character.toUpperCase(decryptedChar);
-//            }
-//            result.append(decryptedChar);
-//        }
-//        return result.toString();
 
-
-//    }
-//}
